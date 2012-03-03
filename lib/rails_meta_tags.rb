125e0ea87d076ecd49f5bc0c38725c90e3786e24
Mateo Murphy <mateo.murphy@gmail.com>
2012-03-03 18:41:42 -0500

    Selectively require active_support

--- lib/rails_meta_tags.rb
+++ lib/rails_meta_tags.rb
@@ -1,3 +1,5 @@
+require 'active_support/concern'
+require 'active_support/core_ext/module'
 require 'rails/meta_tags'
 
-ActionController::Base.send :include, Rails::MetaTags::ControllerMethods
+ActionController::Base.send :include, Rails::MetaTags::ControllerMethods if defined?(ActionController)
