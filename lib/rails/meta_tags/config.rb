e7840db8871555c7ffa4106792d7d4bf89e3df22
Mateo Murphy <mateo.murphy@gmail.com>
2012-03-03 22:49:37 -0500

    Extract config object

---
+++ lib/rails/meta_tags/config.rb
@@ -0,0 +1,30 @@
+module Rails
+  module MetaTags
+    class Config
+      PROPERTIES.each do |t|
+        class_eval "def #{t}(val = nil, &block); self[:#{t}] = val || block; end"
+      end
+
+      def initialize(data = nil)
+        # defaults
+        @data = data || {}
+      end
+
+      def configure_via_block(block)
+        instance_eval(&block)
+      end      
+    
+      def [](tag)
+        @data[tag.to_sym]
+      end
+    
+      def []=(tag, value)
+        @data[tag.to_sym] = value
+      end
+    
+      def to_hash
+        @data.dup
+      end
+    end
+  end
+end
--- lib/rails/meta_tags/model_support/data_config.rb
+++ lib/rails/meta_tags/model_support/data_config.rb
@@ -1,36 +1,10 @@
 module Rails
   module MetaTags
     module ModelSupport
-      class DataConfig
-        def initialize(data = nil)
-          # defaults
-          @data = data || {}
-        end
-
-        def configure_via_block(block)
-          instance_eval(&block)
-        end      
-
-        PROPERTIES.each do |t|
-          class_eval "def #{t}(val = nil, &block); self[:#{t}] = val || block; end"
-        end
-    
-        def [](tag)
-          @data[tag]
-        end
-    
-        def []=(tag, value)
-          @data[tag] = value
-        end
-    
+      class DataConfig < Config
         def instance(model)
           @instance ||= Data.new(self, model)
         end
-    
-        def to_hash
-          @data.dup
-        end
-    
       end
     end
   end
