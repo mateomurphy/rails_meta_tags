6b3439390fe7c413232e7021d36b238ba1c52f97
Mateo Murphy <mateo.murphy@gmail.com>
2012-03-03 22:49:19 -0500

    Automatically include model support

--- lib/rails_meta_tags.rb
+++ lib/rails_meta_tags.rb
@@ -4,3 +4,4 @@ require 'active_support/core_ext/module'
 require 'rails/meta_tags'
 
 ActionController::Base.send :include, Rails::MetaTags::ControllerMethods if defined?(ActionController)
+ActiveRecord::Base.send :include, Rails::MetaTags::ModelSupport if defined?(ActiveRecord)
